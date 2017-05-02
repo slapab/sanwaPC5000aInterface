2b01fe10e7ee0d0271acdd269ee00347db683b4f
slapab <sla.pab.dev@gmail.com>
2017-05-02 23:42:52 +0200

    Cleanup and updating code to take use of hardware version informations.

--- firmware/application/ir_interface.c
+++ firmware/application/ir_interface.c
@@ -7,51 +7,94 @@
 #include "ir_interface.h"
 #include "systick_local.h"
 
-#define GPIO_PORT    GPIOB
-#define GPIO_DATA_IN GPIO4
-#define GPIO_DATA_CLK GPIO3
+#if INTERFACE_VER1 == USING_INTERFACE_VER
 
-/// todo:comment
+#define GPIO_PORT               GPIOB
+#define GPIO_DATA_CLK           GPIO3
+#define GPIO_DATA_IN            GPIO4
+/// Number of pin that is used to receive data
+#define GPIO_DATA_IN_PIN_NO     4
+/// Used timer to generate start impulse and clock signal. See doc. of TIMER of libopencm3.
+#define USED_TIMER_PERIPH       TIM2
+/// Output compare channel number. See doc. of TIMER of libopencm3.
+#define USED_TIMER_OC_CHANNEL   TIM_OC2
+/// Timer Compare/Capture interrupt enable bit. See of "TIMx_DIER Timer DMA and Interrupt Enable Values" of libopencm3.
+#define USED_TIMER_DIER_CCIE    TIM_DIER_CC2IE
+/// NVIC IRQ number that is assigned to used timer. See doc. of NVIC of libopencm3.
+#define USED_TIMER_NVIC_IRQ     NVIC_TIM2_IRQ
+/// EXTI source that is using to detect the DMM readiness. See doc. of EXIT of libopencm3.
+#define USED_EXTI_SOURCE        EXTI4
+/// NVIC IRQ number that is assigned to used EXTI source. See doc. of NVIC of libopencm3.
+#define USED_EXTI_NVIC_IRQ      NVIC_EXTI4_IRQ
+/// Defines the EXTI trigger type. See doc. of EXTI of libopencm3.
+#define USED_EXTI_TRIGGER_TYPE  EXTI_TRIGGER_RISING
+
+#endif
+
+/**
+ * Timer's registers configuration values which are using to generate 10ms-long level on the output pin.
+ *
+ * These values were counted with assumptions:
+ * - frequency of the internal timer's clock: 48 MHz
+ * - runs in pwm - mode 1
+ * - required high level time - 10ms
+ * - period as short as possible (high level time plus something small to get integer value)
+*/
 #define TIM_PULSE_GEN_PRESCALER 7999
 #define TIM_PULSE_GEN_OCCR      60
 #define TIM_PULSE_GEN_ARR       65
 
-/// todo: comment
+/**
+ * Timer's registers configuration values which are using to generate 'clk' signal on the output pin.
+ *
+ * These value were counted with assumptions:
+ * - frequency of the internal timer's clock: 48 MHz
+ * - runs in pwm - mode 1
+ * - duty: 50%
+ * - th = tl = not less than 2us
+ */
 #define TIM_CLK_GEN_PRESCALER   39
 #define TIM_CLK_GEN_OCCR        100
 #define TIM_CLK_GEN_ARR         199
 
 
-/// todo comment
-#define GPIOB_IDR_ADDR (GPIOB+0x08)
+/// Defines offset of the Input Data Register (IDR) from the GPIO base address.
+#define GPIO_IDR_OFFSET 0x08
+/// Defines address of the Input Data REgister (IDR) for given GPIO port.
+#define GPIO_IDR_ADDR(gpio_port) ((gpio_port)+GPIO_IDR_OFFSET)
+/// How many bits need to read from DMM during one transaction.
+#define DMM_DATA_BITS_LEN (IR_DATA_BYTES*8)
+/// Macro that assembles all possible interrupt flags that can be cleared in TIM's status register
+#define TIM_SR_ALL_INT_FLAGS (TIM_SR_BIF | TIM_SR_CC1IF | TIM_SR_CC2IF | TIM_SR_CC3IF | TIM_SR_CC4IF | \
+                              TIM_SR_COMIF | TIM_SR_TIF | TIM_SR_UIF)
 
-#define DMM_DATA_BITS_LEN (IR_DATA_LEN*8)
 
-
-/// todo comment
+/// Configures timer to generate start impulse.
 static void ir_itf_generate_start_pulse(void);
-/// todo comment
+
+/** Configures Output Compare of given timer to use PWM1 mode.
+ *
+ *  @param timer_peripheral  Timer register address base (see doc of libopencm3).
+ *  @param oc_channel        OC channel designators TIM_OCx where x=1..4 (see doc. of libopencm3).
+ *  @param ccr_value         Compare value.
+ */
 static void configure_tim_oc(const uint32_t timer_peripheral, enum tim_oc_id oc_channel, const uint16_t ccr_value);
-/// todo comment
-static void configure_tim(const uint32_t timer_peripheral);
-/// todo comment
+
+/// Configures exti to catch signal from the DMM when it is ready to transmit data.
 static void configure_exti_for_data_ready_signal(void);
 
 
-// todo : comment of private variables
+/// This variable points to the bit-band region which stores the value of current bit on 'data in' pin.
 static volatile uint32_t* dataInBit = NULL;
-// todo: comment
+/// Using in the timer interrupt to track the receiving bit number.
 static volatile uint8_t bitNo = 0;
+/// Using in the timer interrupt to track the receiving byte number.
 static volatile uint8_t byteNo = 0;
+/// Points to the active buffer where receiving data are storing.
 static uint8_t* pActiveBuf = NULL;
 
-// todo state
-// 0 - ready
-// 1 - working
-#define STATE_READY 0
-#define STATE_WORKING 1
-#define STATE_DONE 2
-static volatile sig_atomic_t dmmCommState = STATE_READY;
+
+static volatile sig_atomic_t dmmCommState = IR_ITF_READY;
 
 void ir_itf_init_blocking(void) {
     rcc_periph_clock_enable(RCC_GPIOB);
@@ -67,7 +110,7 @@ void ir_itf_init_blocking(void) {
 bool ir_itf_read_blocking(uint8_t* const buffer, const size_t len) {
     bool retval = false;
 
-    if ((NULL != buffer) && (len >= IR_DATA_LEN)) {
+    if ((NULL != buffer) && (len >= IR_DATA_BYTES)) {
     do {
         // preconditions -> IR receiver must giving at this moment '0'
         if (0 != gpio_get(GPIO_PORT, GPIO_DATA_IN)) {
@@ -90,8 +133,8 @@ bool ir_itf_read_blocking(uint8_t* const buffer, const size_t len) {
         }
 
         // start generating 128 impulses on data clock and read data on falling edge
-        for (int byteNo = 0; byteNo < IR_DATA_LEN; ++byteNo) {
-            for (int bitNo = 0; bitNo < 8; ++bitNo) {
+        for (int byteNO = 0; byteNO < IR_DATA_BYTES; ++byteNO) {
+            for (int bitNO = 0; bitNO < 8; ++bitNO) {
                 gpio_set(GPIO_PORT, GPIO_DATA_CLK);
                 //nops_wait();nops_wait();nops_wait();nops_wait();
                 st_delay_ms(1);
@@ -100,7 +143,7 @@ bool ir_itf_read_blocking(uint8_t* const buffer, const size_t len) {
                 uint16_t bitVal = gpio_get(GPIO_PORT, GPIO_DATA_IN); // 0 or 1
                 // insert just read bit into right place of buffer
                 // this is branching less replacement for: if(bitVal == 1) buffer[byteNo] |= (1<<bitNo); else buffer[byteNo] &= ~(1<<bitNo);
-                buffer[byteNo] ^= (-bitVal ^ buffer[byteNo]) & (1 << bitNo);
+                buffer[byteNO] ^= (-bitVal ^ buffer[byteNO]) & (1 << bitNO);
 
 //                nops_wait();nops_wait();nops_wait();nops_wait();
                 st_delay_ms(1);
@@ -119,7 +162,7 @@ void ir_itf_init_nb(void) {
     rcc_periph_clock_enable(RCC_GPIOB);
     rcc_periph_clock_enable(RCC_AFIO);
 
-    // enable clock for TIM2
+    // enable clock for USED_TIMER_PERIPH
     rcc_periph_clock_enable(RCC_TIM2);
 
     // reset tim2 peripheral to defaults
@@ -130,19 +173,21 @@ void ir_itf_init_nb(void) {
 
     // configure clk pin -> alternate function -> using as PWM output
     gpio_set_mode(GPIO_PORT, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_DATA_CLK);
-    // remap to TIM2-CH2 to PB3
+
+#if INTERFACE_VER1 == USING_INTERFACE_VER
+    // remap TIM2-CH2 to PB3
     gpio_primary_remap(AFIO_MAPR_SWJ_CFG_JTAG_OFF_SW_ON, AFIO_MAPR_TIM2_REMAP_PARTIAL_REMAP1);
+#endif
 
-    timer_reset(TIM2);
+    timer_reset(USED_TIMER_PERIPH);
 
-
-    // map vale of (bit) of DATA_IN pin to variable (bit banding) | fixme: magic number!
-    dataInBit = &BBIO_PERIPH((GPIOB_IDR_ADDR), 4);
+    // maps bit vale of DATA_IN pin to variable (bit banding)
+    dataInBit = &BBIO_PERIPH((GPIO_IDR_ADDR(GPIO_PORT)), GPIO_DATA_IN_PIN_NO);
 }
 
 
 void ir_itf_start_read_nb(uint8_t* const buffer, const size_t len) {
-    if (NULL == buffer || len < IR_DATA_LEN || STATE_WORKING == dmmCommState) {
+    if (NULL == buffer || len < IR_DATA_BYTES || IR_ITF_WORKING == dmmCommState) {
         return;
     }
 
@@ -151,18 +196,18 @@ void ir_itf_start_read_nb(uint8_t* const buffer, const size_t len) {
     byteNo = 0;
     pActiveBuf = buffer;
 
-    dmmCommState = STATE_WORKING;
+    dmmCommState = IR_ITF_WORKING;
     ir_itf_generate_start_pulse();
 }
 
-int ir_itf_get_status(void) {
-    int isWorking = 1;
+ir_itf_state_type ir_itf_get_status(void) {
+    ir_itf_state_type isWorking = IR_ITF_WORKING;
 
-    if (STATE_DONE == dmmCommState) {
-        dmmCommState = STATE_READY;
-        isWorking = 2;
-    } else if (STATE_READY == dmmCommState) {
-        isWorking = 0;
+    if (IR_ITF_DONE == dmmCommState) {
+        dmmCommState = IR_ITF_READY;
+        isWorking = IR_ITF_DONE;
+    } else if (IR_ITF_READY == dmmCommState) {
+        isWorking = IR_ITF_READY;
     }
 
     return isWorking;
@@ -176,62 +221,65 @@ static void configure_tim_oc(const uint32_t timer_peripheral, enum tim_oc_id oc_
     timer_enable_oc_output(timer_peripheral, oc_channel);
 }
 
-static void configure_tim(const uint32_t timer_peripheral) {
-    timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-}
 
 static void configure_exti_for_data_ready_signal(void) {
     // configure the EXTI subsystem
-    exti_select_source(EXTI4, GPIO_PORT);
+    exti_select_source(USED_EXTI_SOURCE, GPIO_PORT);
     // rising edge when receiving signal from DMM
-    exti_set_trigger(EXTI4, EXTI_TRIGGER_RISING);
-    exti_enable_request(EXTI4);
+    exti_set_trigger(USED_EXTI_SOURCE, USED_EXTI_TRIGGER_TYPE);
+    exti_enable_request(USED_EXTI_SOURCE);
 
-    // enable EXTI4 interrupt
-    nvic_clear_pending_irq(NVIC_EXTI4_IRQ);
-    nvic_enable_irq(NVIC_EXTI4_IRQ);
+    // enable USED_EXTI_SOURCE interrupt
+    nvic_clear_pending_irq(USED_EXTI_NVIC_IRQ);
+    nvic_enable_irq(USED_EXTI_NVIC_IRQ);
 }
 
 static void ir_itf_generate_start_pulse(void) {
     // need to configure timer into PWM with one shot mode to generate the
-    nvic_disable_irq(NVIC_TIM2_IRQ);
+    nvic_disable_irq(USED_TIMER_NVIC_IRQ);
 
-    // reset tim2 peripheral to defaults
-    timer_reset(RST_TIM2);
+    // reset USED_TIMER_PERIPH peripheral to defaults
+    timer_reset(USED_TIMER_PERIPH);
 
-    // TIM2 uses APB1 and with current settings it has prescaler set to 2, but clock used by TIM2 is doubled in that
+    // USED_TIMER_PERIPH uses APB1 and with current settings it has prescaler set to 2, but clock used by TIM2 is doubled in that
     // case
-    configure_tim(TIM2);
-    timer_set_prescaler(TIM2, TIM_PULSE_GEN_PRESCALER);
-    timer_enable_preload(TIM2);
-    timer_one_shot_mode(TIM2);
-    timer_set_period(TIM2, TIM_PULSE_GEN_ARR);
-    configure_tim_oc(TIM2, TIM_OC2, TIM_PULSE_GEN_OCCR);
+    timer_set_mode(USED_TIMER_PERIPH, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+    timer_set_prescaler(USED_TIMER_PERIPH, TIM_PULSE_GEN_PRESCALER);
+    timer_enable_preload(USED_TIMER_PERIPH);
+    timer_one_shot_mode(USED_TIMER_PERIPH);
+    timer_set_period(USED_TIMER_PERIPH, TIM_PULSE_GEN_ARR);
+    configure_tim_oc(USED_TIMER_PERIPH, USED_TIMER_OC_CHANNEL, TIM_PULSE_GEN_OCCR);
     // software generate UpdateEvent to apply values in shadowed registers
-    timer_generate_event(TIM2, TIM_EGR_UG);
-    TIM_SR(TIM2) = 0; // clearing all flags in status register (clear on write '0')
+    timer_generate_event(USED_TIMER_PERIPH, TIM_EGR_UG);
+    // clearing all flags in status register (clear on write '0')
+    timer_clear_flag(USED_TIMER_PERIPH, TIM_SR_ALL_INT_FLAGS);
 
-    timer_enable_counter(TIM2);
+    timer_enable_counter(USED_TIMER_PERIPH);
 
     // Counter works in one shot mode, which means: UE will occur and counter will be disabled, but output compare stage
     // will set HIGH state on pin due to reloaded values in registers. This will turn on IR LED - what is undesirable.
     // This write sets 0 on compare register to force output level to be low when UE occur.
     // Because counter is enabled this write take effect only on UpdateEvent.
-    timer_set_oc_value(TIM2, TIM_OC2, 0);
+    timer_set_oc_value(USED_TIMER_PERIPH, USED_TIMER_OC_CHANNEL, 0);
 
     // configure exti on DATA INPUT pin on rising edge -> this will be an event when DMM is ready to transmit data
     configure_exti_for_data_ready_signal();
 }
 
 
-
+/**
+ * On each compare match only when generating CLK signal. This interrupt represents the falling edge of clock which
+ * is also a data sampling edge.
+ */
 __attribute__((interrupt)) void tim2_isr(void) {
-    if (true == timer_interrupt_source(TIM2, TIM_SR_CC2IF)) { // Falling edge of CLK signal -> edge of sampling
-        timer_clear_flag(TIM2, TIM_SR_CC2IF);
+    if (true == timer_interrupt_source(USED_TIMER_PERIPH, TIM_SR_CC2IF)) { // Falling edge of CLK signal -> edge of sampling
+        timer_clear_flag(USED_TIMER_PERIPH, TIM_SR_CC2IF);
         //
         // Note about assumption: byteNo and bitNo must be set to 0 prior first interrupt occur
         //
-        uint16_t bitVal = *dataInBit; // Read 0 or 1 from bit-band region
+
+        // Read 0 or 1 from bit-band region of IDR of given GPIO pin
+        uint16_t bitVal = *dataInBit;
 
         // insert just read bit into right place of buffer
         // this is branching less replacement for: if(bitVal == 1) buffer[byteNo] |= (1<<bitNo); else buffer[byteNo] &= ~(1<<bitNo);
@@ -244,47 +292,53 @@ __attribute__((interrupt)) void tim2_isr(void) {
             bitNo = 0;
             ++byteNo;
             // check if that was a last byte
-            if (byteNo >= IR_DATA_LEN) {
+            if (byteNo >= IR_DATA_BYTES) {
                 // that is all, force output to be low, disable counter, set appropriate flag that informs about finished
                 // data reading
-                timer_set_oc_mode(TIM2, TIM_OC2, TIM_OCM_FORCE_LOW);
-                timer_disable_irq(TIM2, TIM_DIER_CC2IE);
-                timer_disable_counter(TIM2);
+                timer_set_oc_mode(USED_TIMER_PERIPH, USED_TIMER_OC_CHANNEL, TIM_OCM_FORCE_LOW);
+                timer_disable_irq(USED_TIMER_PERIPH, USED_TIMER_DIER_CCIE);
+                timer_disable_counter(USED_TIMER_PERIPH);
 
-                nvic_disable_irq(NVIC_TIM2_IRQ);
-                nvic_clear_pending_irq(NVIC_TIM2_IRQ);
+                nvic_disable_irq(USED_TIMER_NVIC_IRQ);
+                nvic_clear_pending_irq(USED_TIMER_NVIC_IRQ);
 
-                dmmCommState = STATE_DONE;
+                dmmCommState = IR_ITF_DONE;
             }
         }
     } // TIM_SR_CC2IF
 } // tim2_isr()
 
+
+/**
+ * Interrupt occurs when DMM indicates (by turning its IR LED on) when it is read to transmit data.
+ */
 __attribute__((interrupt)) void exti4_isr(void) {
-    exti_reset_request(EXTI4);
+    exti_reset_request(USED_EXTI_SOURCE);
     // disable exti
-    exti_disable_request(EXTI4);
-    nvic_disable_irq(NVIC_EXTI4_IRQ);
-    nvic_clear_pending_irq(NVIC_EXTI4_IRQ);
+    exti_disable_request(USED_EXTI_SOURCE);
+    nvic_disable_irq(USED_EXTI_NVIC_IRQ);
+    nvic_clear_pending_irq(USED_EXTI_NVIC_IRQ);
 
     // re-setup TIMER to generate clock for 128 bits of data from DMM
     // data will be read on falling edge
-    timer_disable_counter(TIM2);
-    nvic_disable_irq(NVIC_TIM2_IRQ);
+    timer_disable_counter(USED_TIMER_PERIPH);
+    nvic_disable_irq(USED_TIMER_NVIC_IRQ);
 
-    timer_set_prescaler(TIM2, TIM_CLK_GEN_PRESCALER);
-    timer_set_period(TIM2, TIM_CLK_GEN_ARR);
-    timer_set_oc_value(TIM2, TIM_OC2, TIM_CLK_GEN_OCCR);
-    timer_continuous_mode(TIM2);
+    timer_set_prescaler(USED_TIMER_PERIPH, TIM_CLK_GEN_PRESCALER);
+    timer_set_period(USED_TIMER_PERIPH, TIM_CLK_GEN_ARR);
+    timer_set_oc_value(USED_TIMER_PERIPH, USED_TIMER_OC_CHANNEL, TIM_CLK_GEN_OCCR);
+    timer_continuous_mode(USED_TIMER_PERIPH);
 
     // software generate UpdateEvent to apply values in shadowed registers
-    timer_generate_event(TIM2, TIM_EGR_UG);
-    TIM_SR(TIM2) = 0; // clearing all flags in status register (clear on write '0')
-    timer_enable_irq(TIM2, TIM_DIER_CC2IE);
+    timer_generate_event(USED_TIMER_PERIPH, TIM_EGR_UG);
+    // clearing all flags in status register (clear on write '0')
+    timer_clear_flag(USED_TIMER_PERIPH, TIM_SR_ALL_INT_FLAGS);
+    // Trigger interrupt on compare match -> this will be the falling edge of the clock signal -> sampling edge
+    timer_enable_irq(USED_TIMER_PERIPH, USED_TIMER_DIER_CCIE);
 
-    nvic_clear_pending_irq(NVIC_TIM2_IRQ);
-    nvic_enable_irq(NVIC_TIM2_IRQ);
+    nvic_clear_pending_irq(USED_TIMER_NVIC_IRQ);
+    nvic_enable_irq(USED_TIMER_NVIC_IRQ);
 
     // start counting
-    timer_enable_counter(TIM2);
-}
+    timer_enable_counter(USED_TIMER_PERIPH);
+} // exti4_isr()
--- firmware/application/ir_interface.h
+++ firmware/application/ir_interface.h
@@ -6,34 +6,59 @@
 #include <stddef.h>
 
 /// Length of buffer (in bytes) required to store data read with IR interface
-#define IR_DATA_LEN 16
+#define IR_DATA_BYTES 16
+
+
+typedef enum {
+    IR_ITF_READY = 0,
+    IR_ITF_WORKING,
+    IR_ITF_DONE
+} ir_itf_state_type;
 
 /**
- * Initializes I/O
+ * Initializes I/O for blocking data reading transaction.
+ *
+ * @note DMM requires a few milliseconds of the start impulse and then need to wait of dozens milliseconds until it will
+ * be ready to transmit.
  */
 void ir_itf_init_blocking(void);
 
 /**
  * Reads data from DMM - this is blocking operation.
  *
- * @param [in,out] buff pointer to memory where read data will be stored
- * @param [in] len length of providing buffer
+ * @param[in,out] buff pointer to memory where read data will be stored
+ * @param[in] len length of providing buffer
  * @return true if read operations was successful, false otherwise
+ *
+ * @note DMM requires a few milliseconds of the start impulse and then need to wait of dozens milliseconds until it will
+ * be ready to transmit. Transaction will read 128 bits when period of clock is 2ms.
  */
 bool ir_itf_read_blocking(uint8_t* const buffer, const size_t len);
 
-
-/// todo comment
+/**
+ * Initializes I/O and timer used during non-blocking data transmission from the DMM.
+ */
 void ir_itf_init_nb(void);
 
-/// todo comment
+/** todo return type to distinguish if starting was successful
+ * Begins (if not busy) non-blocking raw data transmission from the DMM. After calling this function the whole process
+ * is performed automatically. To check current transmission state use \ref todo: point to proper function!
+ *
+ * The reading process consist of few steps:
+ * 1. Generating impulse (few milliseconds) to request data from the DMM.
+ * 2. When DMM is ready to transmit, turns on its IR LED (exti event).
+ * 3. Then the timer configured in PWM mode generates clock cycles to receive 128 bits of data. Reading data is performed
+ *    on clock's falling edge.
+ *
+ * @param[out] buffer   pointer to the buffer where received data from DMM will be stored.
+ * @param[in]  len      length of the buffer. Can not be less than \ref IR_DATA_LEN.
+ *
+ * @note DMM turns on its IR LED when sends '0' bit.
+ */
 void ir_itf_start_read_nb(uint8_t* const buffer, const size_t len);
 
-int ir_itf_get_status(void);
-/**
- * Starts reading data from DMM - this is non blocking blocking operations. It will use SPI peripheral and DMA to
- * perform ...
- */
-//bool ir_itf_start_read(uint8_t* const buffer, const size_t len /*timer*/);
+// todo something to check status and check if can start reading data from the DMM.
+ir_itf_state_type ir_itf_get_status(void);
+
 
 #endif //IR_INTERFACE_H_
